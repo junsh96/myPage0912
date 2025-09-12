cd387b45e09ffb956b99ff339900f96c2ee52ad1
junsh96 <junsh96@naver.com>
2025-09-12 18:25:48 +0900

    templete 커밋 및 스펠링 수정

---
+++ HTML/templete.js
@@ -0,0 +1,11 @@
+    $(document).ready(function (){
+        $("#header").load("header.html", function () {
+                
+        })
+        $("#sideBar").load("sideBar.html", function () {
+                
+        })
+         $("#footer").load("footer.html", function () {
+                
+        })
+    });
